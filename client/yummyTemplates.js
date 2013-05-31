bcbc1b3c0a1fdcf0ae050b7e5235f69398a9f372
Tyson Daugherty <Tyson@TYSONs-MacBook-Pro.local>
2013-05-30 21:46:25 -0700

    edit slide place holder added. path to return to slide creations post slide preview.

--- client/yummyTemplates.js
+++ client/yummyTemplates.js
@@ -135,30 +135,6 @@ Template.yummy_coins.events({
   },
 
   //>>>>>>>>>> SHOW MAKER HELPERS <<<<<<<<<<<<<<<<<
-  'click .slidelink1': function(){
-    $('.slide-inputs').remove();
-    $('.saved-slide-preview').remove();
-    var slideTitle = Shows.find({}).fetch();
-    console.log(slideTitle);
-    var type = slideTitle[0]['3']['slideType'];
-    var source = slideTitle[0]['4']['dataSource'];
-    console.log(type);
-    if (type === "chart" && source === "bitcoin") {
-      // var func = slideTitle[1]['2']['contents'];
-      return Meteor.call('D3testinit'); 
-    } else if (type === "chart" && source === "bitly") {
-      return Meteor.call('bitlyLineChartD3'); 
-    } else {
-      var slideTextArray = slideTitle[0][2]['contents'];
-      var title = slideTextArray[0]['text'];
-      var firstBull = slideTextArray[1]['text'];
-      var secondBull = slideTextArray[2]['text'];
-      var thirdBull = slideTextArray[3]['text'];
-      $('.make-start').append('<div id="preview-slide-inputs" class="span12"></div>');
-      // $('#preview-slide-inputs').append('<div class="preview-title-slide-title"></div>'); 
-      $('#preview-slide-inputs').append('<div class="preview-title-slideTitle"> <h1>' + title + '</h1></div><div class="title-sub-title"><h2>' + firstBull + '</h2></div><div class="title-sub-sub"> <h3>' + secondBull + '</h3></div><div class="bullet-third-slide-one"> <h2>' + thirdBull + '</h2></div></div>');
-    }
-  },
   'click .slidelink2': function(){
     $('.slide-inputs').remove();
     $('.saved-slide-preview').remove();
@@ -680,6 +656,36 @@ Template.yummy_coins.events({
       $('#slide-nav-row').append('<div id="slide-controls" class="span12"><span class="make-slide"><p class="make-first-slide"> Save Slide and Continue </p></span></div>');
       return bulletTwo;
     }
+  },
+  'click .slidelink1': function(){
+    $('.slide-inputs').remove();
+    $('.saved-slide-preview').remove();
+    $('#create-chart-sub').remove();
+    var slideTitle = Shows.find({}).fetch();
+    console.log(slideTitle);
+    var type = slideTitle[0]['3']['slideType'];
+    var source = slideTitle[0]['4']['dataSource'];
+    console.log(type);
+    if (type === "chart" && source === "bitcoin") {
+      // var func = slideTitle[1]['2']['contents'];
+      return Meteor.call('D3testinit'); 
+    } else if (type === "chart" && source === "bitly") {
+      return Meteor.call('bitlyLineChartD3'); 
+    } else {
+      var slideTextArray = slideTitle[0][2]['contents'];
+      var title = slideTextArray[0]['text'];
+      var firstBull = slideTextArray[1]['text'];
+      var secondBull = slideTextArray[2]['text'];
+      var thirdBull = slideTextArray[3]['text'];
+      $('#slide-nav-row').append('<div id="edit-current-slide" class="span4"><span class="edit-slide"><p class="edit-this-slide"> Edit This Slide </p></span></div><div id="create-text-sub" class="span4"> <span class="text-slide-sub"><p>Switch to Create Text Slide without saving </p></span></div><div id="create-chart-sub" class="span4"> <span class="chart-slide-sub"><p>Switch to Create Chart Slide without saving </p></span></div>');
+      $('.make-start').append('<div id="preview-slide-inputs" class="span12"></div>');
+      //$('#slide-nav-row').append('<div id="slide-controls" class="span6"><span class="make-slide"><p class="make-first-slide"> Save Slide and Continue </p></span></div><div id="create-chart-sub" class="span6"> <span class="chart-slide-sub"><p>Switch to Create Chart Slide without saving </p></span></div>');
+      // $('#preview-slide-inputs').append('<div class="preview-title-slide-title"></div>'); 
+      $('#preview-slide-inputs').append('<div class="preview-title-slideTitle"> <h1>' + title + '</h1></div><div class="title-sub-title"><h2>' + firstBull + '</h2></div><div class="title-sub-sub"> <h3>' + secondBull + '</h3></div><div class="bullet-third-slide-one"> <h2>' + thirdBull + '</h2></div></div>');
+    }
+  },
+  'click #edit-current-slide': function () {
+    alert('oh yeah bitchy bitch. this whole thing is getting closer to the promise land');
   }
 })
 
